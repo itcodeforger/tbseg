a9a41b23c599f9e820ea90aeced8557f4b3213cd
ng-coder <szudera.rafal@gmail.com>
2016-04-14 00:20:29 +0200

    added random generated map on grid

--- app/modules/procedural/controller/procedural-compiled.js
+++ app/modules/procedural/controller/procedural-compiled.js
@@ -12,40 +12,57 @@ angular.module('myApp.procedural', ['ngRoute']).config(['$routeProvider', functi
   var boardSize = 20;
   var boardResolution = 20;
   var canvasSize = boardSize * boardResolution;
+  var canvas = document.getElementById('tutorial');
+  var ctx = canvas.getContext('2d');
+
+  ctx.canvas.width = canvasSize;
+  ctx.canvas.height = canvasSize;
 
   $scope.$on('$viewContentLoaded', function () {
-    createBoard(boardSize, boardSize, 0);
-    draw(canvasSize, boardResolution);
+    drawGrid(canvasSize, boardResolution);
+    drawMap(createMap(boardSize, boardSize));
   });
 
-  function createBoard(rows, cols, defaultValue) {
+  function createMap(rows, cols) {
     var arr = [];
     for (var i = 0; i < rows; i++) {
       arr.push([]);
       arr[i].push(new Array(cols));
       for (var j = 0; j < cols; j++) {
-        arr[i][j] = defaultValue;
+        arr[i][j] = randomIntFromInterval(0, 1);
       }
     }
     return arr;
   };
 
-  function draw(size, resolution) {
-    var canvas = document.getElementById('tutorial');
-    if (canvas.getContext) {
-      var ctx = canvas.getContext('2d');
-      ctx.canvas.width = canvasSize;
-      ctx.canvas.height = canvasSize;
-
-      for (var i = 0; i <= size; i = i + resolution) {
-        ctx.moveTo(i, 0);
-        ctx.lineTo(i, size);
-        ctx.moveTo(0, i);
-        ctx.lineTo(size, i);
-      }
-      ctx.stroke();
+  function drawGrid(size, resolution) {
+    for (var i = 0; i <= size; i = i + resolution) {
+      ctx.moveTo(i, 0);
+      ctx.lineTo(i, size);
+      ctx.moveTo(0, i);
+      ctx.lineTo(size, i);
     }
+    ctx.stroke();
   };
+
+  function drawArea(x, y) {
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect(x, y, boardResolution, boardResolution);
+  }
+
+  function drawMap(mapArray) {
+    for (var i in mapArray) {
+      for (var j in mapArray[i]) {
+        if (mapArray[i][j] === 1) {
+          drawArea(i * 20, j * 20);
+        };
+      }
+    }
+  }
+
+  function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 }]);
 
 //# sourceMappingURL=procedural-compiled.js.map
--- app/modules/procedural/controller/procedural.js
+++ app/modules/procedural/controller/procedural.js
@@ -11,42 +11,58 @@ angular.module('myApp.procedural', ['ngRoute'])
     });
   }])
   .controller('proceduralCtrl', ['$scope', ($scope) => {
-    let boardSize = 20;
-    let boardResolution = 20;
-    let canvasSize = boardSize * boardResolution;
+    const boardSize = 20;
+    const boardResolution = 20;
+    const canvasSize = boardSize * boardResolution;
+    const canvas = document.getElementById('tutorial');
+    const ctx = canvas.getContext('2d');
+
+    ctx.canvas.width = canvasSize;
+    ctx.canvas.height = canvasSize;
 
     $scope.$on('$viewContentLoaded', function(){
-      createBoard( boardSize, boardSize, 0);
-      draw(canvasSize, boardResolution);
+      drawGrid(canvasSize, boardResolution);
+      drawMap(createMap( boardSize, boardSize));
     });
 
-    function createBoard( rows, cols, defaultValue) {
+    function createMap( rows, cols) {
       let arr = [];
       for ( let i=0; i < rows; i++ ) {
         arr.push([]);
         arr[i].push( new Array(cols) );
         for ( let j=0; j < cols; j++ ) {
-          arr[i][j] = defaultValue;
+          arr[i][j] = randomIntFromInterval(0,1);
         }
       }
       return arr;
     };
 
-    function draw( size, resolution){
-      var canvas = document.getElementById('tutorial');
-      if (canvas.getContext){
-        var ctx = canvas.getContext('2d');
-        ctx.canvas.width = canvasSize;
-        ctx.canvas.height = canvasSize;
-
-        for (let i=0; i <= size; i = i + resolution){
-          ctx.moveTo(i,0);
-          ctx.lineTo(i,size);
-          ctx.moveTo(0,i);
-          ctx.lineTo(size,i);
-        }
-        ctx.stroke();
+    function drawGrid( size, resolution) {
+      for (let i=0; i <= size; i = i + resolution){
+        ctx.moveTo(i,0);
+        ctx.lineTo(i,size);
+        ctx.moveTo(0,i);
+        ctx.lineTo(size,i);
       }
+      ctx.stroke();
     };
 
+    function drawArea(x, y) {
+      ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+      ctx.fillRect (x, y, boardResolution, boardResolution);
+    }
+
+    function drawMap(mapArray) {
+      for (let i in mapArray) {
+        for(let j in mapArray[i]) {
+          if (mapArray[i][j] === 1) {
+            drawArea(i * 20, j * 20);
+          };
+        }
+      }
+    }
+
+    function randomIntFromInterval(min,max) {
+      return Math.floor(Math.random()*(max-min+1)+min);
+    }
   }]);
