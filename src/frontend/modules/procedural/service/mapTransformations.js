a5d4cf206fc7dc8a24e3077fcdd2f116d8fae8d1
ng-coder <szudera.rafal@gmail.com>
2016-11-20 18:18:45 +0100

    generate darker tile color

--- src/frontend/modules/procedural/service/mapTransformations.js
+++ src/frontend/modules/procedural/service/mapTransformations.js
@@ -22,7 +22,7 @@
         const map = createMap();
         const list = createTileList(map);
         const initialSetup = mm.gatePosition(list, i);
-        const color = 'rgba(' + mo.randomIntFromInterval(0,255) + ', ' + mo.randomIntFromInterval(0,255) + ', ' + mo.randomIntFromInterval(0,255) + ', 0.5)';
+        const color = 'rgba(' + mo.randomIntFromInterval(0,122) + ', ' + mo.randomIntFromInterval(0,122) + ', ' + mo.randomIntFromInterval(0,122) + ', 0.5)';
         board.push(new mapObject(i, config.boardSize, config.boardSize, map, list, initialSetup, color));
       }
       return board;
